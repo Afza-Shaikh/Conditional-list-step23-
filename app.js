b5a45c559e97a98665b689c25c2a8f852b80df41
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:21:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+"use strict";
+let car = 'subaru';
+//5 true tests
+//equality test 
+console.log("Is car == 'subaru' ? I predict True.");
+console.log(car == 'subaru'); //true
+//strict equality test 
+console.log("Is car === 'subaru' ? I predict True.");
+console.log(car === 'subaru'); //True
+//inequality test
+console.log("Is car != 'toyota' ? I predict True.");
+console.log(car != 'toyota'); //True
+// strict inequality test 
+console.log("Is car !== 'toyota' ? I predict True.");
+console.log(car !== 'toyota'); //True
+//equality test
+console.log("Is car == 'subaru' ? I predict True.");
+console.log(car !== 'subaru'); //True
+//equality test
+console.log("Is car == undefined ? I predict false.");
+console.log(car == undefined); //false
+// strict equality test 
+console.log("Is car === 'toyota' ? I predict false.");
+console.log(car === 'toyota'); //false
+// strict equality test 
+console.log("Is car === null ? I predict false.");
+console.log(car === null); //false
+//inequality test
+console.log("Is car != subaru ? I predict false.");
+console.log(car != 'subaru'); //false.
